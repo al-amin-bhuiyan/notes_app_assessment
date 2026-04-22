c0a45f8d4a03fbbafad024b79acdaf89e8128f71
al-amin-bhuiyan <al-amin-bhuiyan@users.noreply.github.com>
2026-04-23 04:56:22 +0600

    App Router configure

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -2,16 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:notes_app/core/constants/route_paths.dart';
-import 'package:notes_app/core/services/user_session_service.dart';
-import 'package:notes_app/features/auth/presentation/bindings/auth_binding.dart';
-import 'package:notes_app/features/auth/presentation/pages/login_page.dart';
-import 'package:notes_app/features/auth/presentation/pages/registration_page.dart';
-import 'package:notes_app/features/notes/presentation/bindings/notes_binding.dart';
-import 'package:notes_app/features/notes/presentation/pages/add_note_page.dart';
-import 'package:notes_app/features/notes/presentation/pages/home_page.dart';
-import 'package:notes_app/features/splash/presentation/bindings/splash_binding.dart';
-import 'package:notes_app/features/splash/presentation/pages/splash_page.dart';
-import 'package:get/get.dart';
+import 'package:notes_app/app/router/app_routes.dart';
 
 class AppRouter {
   AppRouter._();
@@ -19,42 +10,7 @@ class AppRouter {
   static final GoRouter router = GoRouter(
     initialLocation: RoutePaths.splash,
     redirect: _handleRedirect,
-    routes: [
-      GoRoute(
-        path: RoutePaths.splash,
-        builder: (context, state) {
-          SplashBinding().dependencies();
-          return const SplashPage();
-        },
-      ),
-      GoRoute(
-        path: RoutePaths.login,
-        builder: (context, state) {
-          AuthBinding().dependencies();
-          return const LoginPage();
-        },
-      ),
-      GoRoute(
-        path: RoutePaths.register,
-        builder: (context, state) {
-          AuthBinding().dependencies();
-          return const RegistrationPage();
-        },
-      ),
-      GoRoute(
-        path: RoutePaths.home,
-        builder: (context, state) {
-          NotesBinding().dependencies();
-          return const HomePage();
-        },
-      ),
-      GoRoute(
-        path: RoutePaths.addNote,
-        builder: (context, state) {
-          return const AddNotePage();
-        },
-      ),
-    ],
+    routes: AppRoutes.routes,
   );
 
   static String? _handleRedirect(BuildContext context, GoRouterState state) {
